2bd61ce519571e9fde842dbd93f2a02417bddbaa
Henry-baobao <herui1991@live.cn>
2019-05-07 11:32:56 +0800

    first test

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+import WebApp from './WebApp'
+import './index.css';
+
+ReactDOM.render(<WebApp/>,document.getElementById('root'));
